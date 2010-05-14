3e23e0e50f86098df46b5314c980697076b731fb
george <george@84d45830-feb6-42a6-b2cd-1827727d2678>
2010-05-14 04:59:30 +0000

    * Upraveno: Robustnější dekódování z řetězce. * Přidáno: Metoda Assign pro přiřazení hodnoty do jiné proměnné.

--- IPAddress/UIPAddress.pas
+++ IPAddress/UIPAddress.pas
@@ -1,5 +1,7 @@
 unit UIPAddress;
 
+// Date: 2010-05-14
+
 {$mode objfpc}{$H+}
 
 interface
@@ -18,6 +20,7 @@ type
     function GetString: string;
     procedure SetString(Value: string);
     constructor Create;
+    procedure Assign(Source: TObject);
     property AsString: string read GetString write SetString;
   end;
 
@@ -40,27 +43,52 @@ end;
 procedure TIPAddress.SetString(Value: string);
 var
   Part: string;
+  Octet: Integer;
+  TempValue: Integer;
 begin
   if Pos('/', Value) > 0 then begin
-    Prefix := StrToInt(Copy(Value, Pos('/', Value) + 1, Length(Value)));
+    if TryStrToInt(Copy(Value, Pos('/', Value) + 1, Length(Value)), TempValue) then
+      Prefix := TempValue else Prefix := 32;
     Delete(Value, Pos('/', Value), Length(Value));
   end else Prefix := 32;
-  Part := Copy(Value, 1, Pos('.', Value) - 1);
-  Delete(Value, 1, Pos('.', Value));
-  Address := Cardinal(StrToInt(Part) and $ff) shl 24;
-  Part := Copy(Value, 1, Pos('.', Value) - 1);
-  Delete(Value, 1, Pos('.', Value));
-  Address := Address or Cardinal((StrToInt(Part) and $ff) shl 16);
-  Part := Copy(Value, 1, Pos('.', Value) - 1);
-  Delete(Value, 1, Pos('.', Value));
-  Address := Address or Cardinal((StrToInt(Part) and $ff) shl 8);
-  Address := Address or Cardinal((StrToInt(Value) and $ff) shl 0);
+  if Pos('.', Value) > 0 then begin
+    Part := Copy(Value, 1, Pos('.', Value) - 1);
+    Delete(Value, 1, Pos('.', Value));
+    if TryStrToInt(Part, TempValue) then Octet := TempValue
+      else Octet := 0;
+    Address := Address or Cardinal((Octet and $ff) shl 24);
+  end;
+  if Pos('.', Value) > 0 then begin
+    Part := Copy(Value, 1, Pos('.', Value) - 1);
+    Delete(Value, 1, Pos('.', Value));
+    if TryStrToInt(Part, TempValue) then Octet := TempValue
+      else Octet := 0;
+    Address := Address or Cardinal((Octet and $ff) shl 16);
+  end;
+  if Pos('.', Value) > 0 then begin
+    Part := Copy(Value, 1, Pos('.', Value) - 1);
+    Delete(Value, 1, Pos('.', Value));
+    if TryStrToInt(Part, TempValue) then Octet := TempValue
+      else Octet := 0;
+    Address := Address or Cardinal((Octet and $ff) shl 8);
+  end;
+  if TryStrToInt(Value, TempValue) then Octet := TempValue
+    else Octet := 0;
+  Address := Address or Cardinal((Octet and $ff) shl 0);
 end;
 
 constructor TIPAddress.Create;
 begin
   Address := 0;
   Prefix := 32;
+end;
+
+procedure TIPAddress.Assign(Source: TObject);
+begin
+  if Source is TIPAddress then begin
+    Address := TIPAddress(Source).Address;
+    Prefix := TIPAddress(Source).Prefix;
+  end;
 end;
 
 end.
