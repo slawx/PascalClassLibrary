d409f2bf8edf8d353053d5825a09660636c5c909
chronos <chronos@84d45830-feb6-42a6-b2cd-1827727d2678>
2012-04-03 05:30:09 +0000

    * Added: PinConnection CommSocket splitted to CommTCPClient and CommTCPClient units.

---
+++ PinConnection/UCommTCPClient.pas
@@ -0,0 +1,146 @@
+unit UCommTCPClient;
+
+{$mode Delphi}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, blcksock, synsock, UCommPin, UCommon, UThreading,
+  DateUtils;
+
+type
+  TCommTCPClient = class;
+
+  TReceiveDataEvent = procedure(Stream: TMemoryStream) of object;
+
+  { TCommSocketReceiveThread }
+
+  TCommSocketReceiveThread = class(TListedThread)
+  public
+    Parent: TCommTCPClient;
+    Stream: TMemoryStream;
+    procedure Execute; override;
+    constructor Create(CreateSuspended: Boolean;
+      const StackSize: SizeUInt = DefaultStackSize);
+    destructor Destroy; override;
+  end;
+
+  { TCommTCPClient }
+
+  TCommTCPClient = class
+  private
+    FActive: Boolean;
+    FOnReceiveData: TReceiveDataEvent;
+    FReceiveThread: TCommSocketReceiveThread;
+    procedure ReceiveData(Sender: TCommPin; Stream: TStream);
+    procedure SetActive(const AValue: Boolean);
+  public
+    Socket: TTCPBlockSocket;
+    Pin: TCommPin;
+    Address: string;
+    Port: Word;
+    property Active: Boolean read FActive write SetActive;
+    constructor Create;
+    destructor Destroy; override;
+  end;
+
+
+implementation
+
+resourcestring
+  SCantConnectToServer = 'Can''t connect to remote server';
+
+{ TCommTCPClient }
+
+procedure TCommTCPClient.ReceiveData(Sender: TCommPin; Stream:TStream);
+begin
+  if FActive then begin
+    Socket.SendStreamRaw(Stream);
+  end;
+end;
+
+procedure TCommTCPClient.SetActive(const AValue: Boolean);
+begin
+  if FActive = AValue then Exit;
+  FActive := AValue;
+
+  if AValue then begin
+    Socket.Connect(Address, IntToStr(Port));
+    if Socket.LastError <> 0 then begin
+      FActive := False;
+      raise Exception.Create(SCantConnectToServer);
+    end;
+    FReceiveThread := TCommSocketReceiveThread.Create(True);
+
+    FReceiveThread.FreeOnTerminate := False;
+    FReceiveThread.Parent := Self;
+    FReceiveThread.Start;
+  end else begin
+    Socket.CloseSocket;
+    FReceiveThread.Terminate;
+    FReceiveThread.WaitFor;
+    FreeAndNil(FReceiveThread);
+  end;
+end;
+
+constructor TCommTCPClient.Create;
+begin
+  inherited Create;
+  Socket := TTCPBlockSocket.Create;
+  Pin := TCommPin.Create;
+  Pin.OnReceive := ReceiveData;
+end;
+
+destructor TCommTCPClient.Destroy;
+begin
+  Active := False;
+  Socket.Free;
+  Pin.Free;
+  inherited Destroy;
+end;
+
+{ TCommSocketReceiveThread }
+
+procedure TCommSocketReceiveThread.Execute;
+var
+  InBufferUsed: Integer;
+  Buffer: array of Byte;
+begin
+  InBufferUsed := 0;
+  with Parent do begin
+    repeat
+      if InBufferUsed = 0 then Sleep(1);
+        //else Yield;
+      if Assigned(Socket) then
+      with Socket do
+      if CanRead(0) then begin
+        InBufferUsed := WaitingData;
+        if InBufferUsed > 0 then begin
+          SetLength(Buffer, InBufferUsed);
+          RecvBuffer(Buffer, Length(Buffer));
+
+          Stream.Size := Length(Buffer);
+          Stream.Position := 0;
+          Stream.Write(Buffer[0], Length(Buffer));
+          Pin.Send(Stream);
+        end else InBufferUsed := 0;
+      end else InBufferUsed := 0;
+    until Terminated;
+  end;
+end;
+
+constructor TCommSocketReceiveThread.Create(CreateSuspended: Boolean;
+  const StackSize: SizeUInt);
+begin
+  inherited;
+  Stream := TMemoryStream.Create;
+end;
+
+destructor TCommSocketReceiveThread.Destroy;
+begin
+  Stream.Free;
+  inherited;
+end;
+
+end.
+
---
+++ PinConnection/UCommTCPServer.pas
@@ -0,0 +1,198 @@
+unit UCommTCPServer;
+
+{$mode Delphi}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, blcksock, synsock, UCommPin, UCommon, UThreading,
+  DateUtils, SpecializedList;
+
+type
+  TCommTCPServer = class;
+
+  TReceiveDataEvent = procedure(Stream: TMemoryStream) of object;
+
+  { TCommSocketReceiveThread }
+
+  TCommSocketReceiveThread = class(TListedThread)
+  public
+    Parent: TCommTCPServer;
+    Stream: TMemoryStream;
+    procedure Execute; override;
+    constructor Create(CreateSuspended: Boolean;
+      const StackSize: SizeUInt = DefaultStackSize);
+    destructor Destroy; override;
+  end;
+
+  TSocketConnectEvent = procedure(Sender: TCommTCPServer; Pin: TCommPin) of object;
+
+  { TCommTCPServerSession }
+
+  TCommTCPServerSession = class
+  private
+    procedure ReceiveData(Sender: TCommPin; Stream: TStream);
+  public
+    Server: TCommTCPServer;
+    Socket: TTCPBlockSocket;
+    Pin: TCommPin;
+    constructor Create;
+    destructor Destroy; override;
+  end;
+
+  { TCommTCPServer }
+
+  TCommTCPServer = class
+  private
+    FActive: Boolean;
+    FOnConnect: TSocketConnectEvent;
+    FOnDisconnect: TSocketConnectEvent;
+    FOnReceiveData: TReceiveDataEvent;
+    FReceiveThread: TCommSocketReceiveThread;
+    procedure SetActive(const AValue: Boolean);
+  public
+    Sessions: TListObject; // TListObject<TCommTCPServerSession>
+    Socket: TTCPBlockSocket;
+    Address: string;
+    Port: Word;
+    property Active: Boolean read FActive write SetActive;
+    property OnConnect: TSocketConnectEvent read FOnConnect write FOnConnect;
+    property OnDisconnect: TSocketConnectEvent read FOnDisconnect write FOnDisconnect;
+    constructor Create;
+    destructor Destroy; override;
+  end;
+
+implementation
+
+{ TCommTCPServerSession }
+
+procedure TCommTCPServerSession.ReceiveData(Sender: TCommPin; Stream: TStream);
+begin
+  Socket.SendStreamRaw(Stream);
+end;
+
+constructor TCommTCPServerSession.Create;
+begin
+  Socket := TTCPBlockSocket.Create;
+  Pin := TCommPin.Create;
+  Pin.OnReceive := ReceiveData;
+end;
+
+destructor TCommTCPServerSession.Destroy;
+begin
+  Pin.Free;
+  Socket.Free;
+  inherited Destroy;
+end;
+
+{ TCommSocketReceiveThread }
+
+procedure TCommSocketReceiveThread.Execute;
+var
+  InBufferUsed: Integer;
+  Buffer: array of Byte;
+  NewSocket: TSocket;
+  NewSession: TCommTCPServerSession;
+  I: Integer;
+  DoSleep: Boolean;
+begin
+  with Parent do begin
+    repeat
+      DoSleep := True;
+
+      // Check if new connection is available
+      if Assigned(Socket) then
+      with Socket do
+      if CanRead(0) then begin
+        NewSocket := Accept;
+        if Assigned(FOnConnect) then begin
+          NewSession := TCommTCPServerSession.Create;
+          NewSession.Server := Parent;
+          NewSession.Socket.Socket := NewSocket;
+          if Assigned(FOnConnect) then FOnConnect(Parent, NewSession.Pin);
+          Parent.Sessions.Add(NewSession);
+          DoSleep := False;
+        end;
+      end;
+
+      // Check available data on all opened sockets
+      for I := 0 to Sessions.Count - 1 do
+      with TCommTCPServerSession(Sessions[I]) do begin
+        InBufferUsed := Socket.WaitingData;
+        if (not Pin.Connected) or (Socket.Socket = INVALID_SOCKET) or
+        (Socket.LastError <> 0) then begin
+          if Assigned(FOnDisconnect) then FOnDisconnect(Self.Parent, Pin);
+        end;
+
+        if InBufferUsed > 0 then begin
+          SetLength(Buffer, InBufferUsed);
+          Socket.RecvBuffer(Buffer, Length(Buffer));
+          //if Socket.Socket = INVALID_SOCKET then Break;
+
+          Stream.Size := Length(Buffer);
+          Stream.Position := 0;
+          Stream.Write(Buffer[0], Length(Buffer));
+          Pin.Send(Stream);
+          DoSleep := False;
+        end;
+      end;
+      if DoSleep and not Terminated then Sleep(1);
+    until Terminated;
+  end;
+end;
+
+constructor TCommSocketReceiveThread.Create(CreateSuspended: Boolean;
+  const StackSize: SizeUInt);
+begin
+  inherited;
+  Stream := TMemoryStream.Create;
+end;
+
+destructor TCommSocketReceiveThread.Destroy;
+begin
+  Stream.Free;
+  inherited Destroy;
+end;
+
+{ TCommTCPServer }
+
+procedure TCommTCPServer.SetActive(const AValue: Boolean);
+begin
+  if FActive = AValue then Exit;
+  FActive := AValue;
+
+  if AValue then begin
+    Socket.Bind(Address, IntToStr(Port));
+    if Socket.LastError <> 0 then raise Exception.Create('Bind error');
+    Socket.Listen;
+    if Socket.LastError <> 0 then raise Exception.Create('Listen error');
+    FReceiveThread := TCommSocketReceiveThread.Create(True);
+
+    FReceiveThread.FreeOnTerminate := False;
+    FReceiveThread.Parent := Self;
+    FReceiveThread.Start;
+  end else begin
+    Socket.CloseSocket;
+    FReceiveThread.Terminate;
+    FReceiveThread.WaitFor;
+    FreeAndNil(FReceiveThread);
+  end;
+end;
+
+constructor TCommTCPServer.Create;
+begin
+  inherited Create;
+  Sessions := TListObject.Create;
+  Socket := TTCPBlockSocket.Create;
+end;
+
+destructor TCommTCPServer.Destroy;
+begin
+  Active := False;
+  Socket.Free;
+  Sessions.Free;
+  inherited Destroy;
+end;
+
+end.
+
