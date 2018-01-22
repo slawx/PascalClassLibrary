b9665cf46887afe534713e53d1045c8751802e89
chronos <chronos@84d45830-feb6-42a6-b2cd-1827727d2678>
2018-01-22 09:16:49 +0000

    * Modified: Place application executable to main project directory and copy it elsewhere in build.bat script.

--- ProjectTemplates/FileMenuProject/Install/win/build.bat
+++ ProjectTemplates/FileMenuProject/Install/win/build.bat
@@ -1,7 +1,11 @@
 if not defined LAZDIR (
   set LAZDIR=C:\lazarus
 )
-%LAZDIR%\lazbuild.exe --lazarusdir=%LAZDIR% --build-mode="Release" --cpu=i386 --operating-system=Win32 ..\..\Application.lpi
-%LAZDIR%\lazbuild.exe --lazarusdir=%LAZDIR% --build-mode="Release" --cpu=x86_64 --operating-system=Win64 ..\..\Application.lpi
+SET APPNAME=FileMenuProject
+%LAZDIR%\lazbuild.exe --lazarusdir=%LAZDIR% --build-mode="Release" --cpu=i386 --operating-system=Win32 ..\..\%APPNAME%.lpi
+copy ..\..\%APPNAME%.exe ..\..\lib\i386-win32-Release
+%LAZDIR%\lazbuild.exe --lazarusdir=%LAZDIR% --build-mode="Release" --cpu=x86_64 --operating-system=Win64 ..\..\%APPNAME%.lpi
+copy ..\..\%APPNAME%.exe ..\..\lib\x86_64-win64-Release
 
-"c:\Program Files (x86)\Inno Setup 5\ISCC.exe" "Application.iss"
+"c:\Program Files (x86)\Inno Setup 5\ISCC.exe" "%APPNAME%.iss"
+
